22cd9b4c803b829394c2f971b54d9272fa22841b
EkaterinaPushkova <pushkova1666@gmail.com>
2020-12-05 01:40:14 +0300

    fix, clean

--- app/src/main/java/com/example/drumpads/MainActivity.java
+++ app/src/main/java/com/example/drumpads/MainActivity.java
@@ -7,40 +7,20 @@ import android.annotation.SuppressLint;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
-
-//import android.media.AudioManager;
-import android.media.SoundPool;
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
     View p1, p2, p3, p4, p5, p6, p7, p8, p9;
     private MediaPlayer mp1,mp2, mp3, mp4, mp5, mp6, mp7, mp8, mp9;
 
-    private SoundPool sp;
-    private MediaPlayer mp;
-//    private int sound1;
-//    private int sound2;
-//    private int sound3;
-//    private int sound4;
-//
-//    private int sound6;
-//    private int sound7;
-//    private int sound8;
-//    private int sound9;
-//    private int sound00;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        sp = new SoundPool(2, AudioManager.STREAM_MUSIC, 0);
 
         p1 = findViewById(R.id.p1);
         p2 = findViewById(R.id.p2);
@@ -257,56 +237,3 @@ public class MainActivity extends AppCompatActivity {
 
 }
 
-
-//    public void playsound1 (View v) {
-//        sp.play(sound1,1.0f,1.0f,0,0,100f);
-//    }
-//    public void playsound2 (View v) {
-//        sp.play(sound2,1.0f,1.0f,0,0,100f);
-//
-//    }
-//    public void playsound3 (View v) {
-//        sp.play(sound3,1.0f,1.0f,0,0,10f);
-//
-//    }
-//    public void playsound4 (View v) {
-//        sp.play(sound4,1.0f,1.0f,0,0,10f);
-//
-//    }
-//    public void playsound5 (View v) {
-//        sp.play(sound5,1.0f,1.0f,0,0,10f);
-//
-//    }
-//    public void playsound6 (View v) {
-//        sp.play(sound6,1.0f,1.0f,0,0,10f);
-//
-//    }
-//    public void playsound7 (View v) {
-//        sp.play(sound7,1.0f,1.0f,0,0,10f);
-//
-//    }
-//    public void playsound8 (View v) {
-//        sp.play(sound8,1.0f,1.0f,0,0,10f);
-//
-//    }
-//    public void playsound9 (View v) {
-//        sp.play(sound9,1.0f,1.0f,0,0,10f);
-//
-//    }
-//}
-
-
-//w7.setOnClickListener(new View.OnClickListener(){
-//@Override
-//public void onClick(View view){
-//        mp = MediaPlayer.create(MainActivity.this, R.raw.w7);
-//        MyMedia();
-//        }
-//        });
-//
-//        }
-//
-//private void MyMedia(){
-//        mp.start();
-//
-//        }
